c3e484f717b60e1e21928f9e87e81cb510c3a462
Cihad Atınç <atinccihad@gmail.com>
2022-08-30 17:47:17 +0300

    lambda

--- src/lambda_practice/Lambda03.java
+++ src/lambda_practice/Lambda03.java
@@ -2,73 +2,69 @@ package lambda_practice;
 
 import java.nio.file.Files;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Lambda03 {
     //TASK 01 --> Structured Programming ve Functional Programming ile 1'den x'e kadar
     // tamsayilari toplayan bir program create ediniz.
     //Structured(AMELE) Programming
-    static Scanner scan = new Scanner(System.in);
-    static int son;
-    static int sum = 0;
-    static int bas = 1;
-
     public static int toplaAmele(int i) {
-        System.out.println("1'den itibaren toplamak istediginiz sayiyi giriniz");
-        son = scan.nextInt();
-        while (son >= bas) {
-            sum += bas;
-            bas++;
+        int toplam = 0;
+        for (int j = 0; j <= i; j++) {
+            toplam += j;
         }
-        System.out.println(sum);
-        return sum;
+        return toplam;
     }
 
     //TODO Functional(cincix Programming
     public static int toplaCincix(int x) {
-
-        return 0;
+        return IntStream.
+                range(1, x + 1).//1den x e kdar x dahil int degerler alindi
+                        sum();
     }
 
     //TODO TASK 02 --> 1'den x'e kadar cift tamsayilari toplayan bir program create ediniz.
     public static int toplaCift(int x) {
-        return 0;
+        return IntStream.
+                rangeClosed(1, x).//1 den x e kadar int ler alindi x dahil
+                        filter(t -> t % 2 == 0).//akisda ki cift int degerler alindi
+                        sum();
     }
 
     //TASK 03 --> Ilk x pozitif cift sayiyi toplayan program  create ediniz.
     public static int toplaIlkXCift(int x) {
-        return 0;
+        return IntStream.
+                iterate(2, t -> t + 2).//2 den sonsuza kadar elemanlari 2 artirarak akisa alir
+                        limit(x).//akisdaki ilk x degeri alir
+                        sum();
     }
 
     //TODO TASK 04 --> Ilk X pozitif tek tamsayiyi toplayan programi  create ediniz.
     public static int toplaIlkXTek(int x) {
-        return 0;
+        return IntStream.
+                iterate(1, t -> t + 2).//1 den sonsuza kadar elemanlari 2 artirarak akisa alir
+                        limit(x).//akisdaki ilk x degeri alir
+                        sum();
     }
 
     //TASK 05 --> 2'nin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
     public static void ikininIlkXKuvvetPrint(int x) {
+        IntStream.iterate(2, t -> t * 2).limit(x).forEach(Methods::yazdir);
     }
 
     //TASK 06 --> Istenilen bir sayinin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
     public static void isteneSayiIlkXKuvvetPrint(int istenenSayi, int x) {
+        IntStream.iterate(istenenSayi, t -> t * istenenSayi).limit(x).forEach(Methods::yazdir);
     }
 
-    // TODO TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan programi  create ediniz.
+    //TODO TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan programi  create ediniz.
     public static int istenenSayiFactorial(int x) {
-        return 0;
+        return IntStream.rangeClosed(1, x).reduce(1, (t, u) -> t * u);
     }
 
     //TASK 08 --> Istenilen bir sayinin  x. kuvvetini ekrana yazdiran programi  create ediniz.
     public static double xKuvveti(int sayi, int x) {
-        return 0;
+        return Math.pow(sayi, x);//math classina git pow kuvvetini alir
+        //return IntStream.iterate(sayi,t->t*sayi).limit(x).reduce(0,(t,u)->u);
     }
-
-    /*TASK :
-    2 Boyutlu bir Multidimensional array i input kabul edip, arraydeki tüm
-    elemanları bir
-    listeye kopyalayan ve harf sırasına göre yazdıran bir METHOD yazınız.
-    Eg : Input : {{Ali,Veli,Ayse},{Hasan,Can},{Suzan}}
-    Output:[Ali, Ayse, Can, Hasan, Suzan, Veli]
-    */
-    String[][] arr = {{"Ali", "Veli", "Ayse"}, {"Hasan", "Can"}, {"Suzan"}};
-
 }
---
+++ src/lambda_practice/Runner02.java
@@ -0,0 +1,32 @@
+package lambda_practice;
+
+import static lambda_practice.Lambda03.*;
+
+public class Runner02 {
+    public static void main(String[] args) {
+        System.out.println("TASK 01 amele topla-->" + toplaAmele(10));
+        System.out.println("TASK 01 cincix topla-->" + toplaCincix(10));
+        System.out.println("   ***   ");
+        System.out.println("TASK 02 -->" + toplaCift(10));
+        System.out.println("   ***   ");
+        System.out.println("TASK 03 -->" + toplaIlkXCift(10));
+        System.out.println("   ***   ");
+        System.out.println("TASK 04 -->" + toplaIlkXTek(10));
+        System.out.println("   ***   ");
+        System.out.println("TASK 05 -->");
+        ikininIlkXKuvvetPrint(7);
+        System.out.println("   ***   ");
+        System.out.println("TASK 06 -->");
+        isteneSayiIlkXKuvvetPrint(4, 3);
+        System.out.println("   ***   ");
+        isteneSayiIlkXKuvvetPrint(3, 4);
+        System.out.println("   ***   ");
+        System.out.println("TASK 07 -->" + istenenSayiFactorial(5));
+        System.out.println("   ***   ");
+        System.out.println("TASK 08 -->" + xKuvveti(4, 3));
+        System.out.println("TASK 08 -->" + xKuvveti(3, 4));
+        System.out.println("   ***   ");
+
+
+    }
+}
